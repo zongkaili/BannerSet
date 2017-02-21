fcd81b9b0a8a47b94aa9d264e6044fbc80315fac
zongkaili <15868178140@163.com>
2017-02-21 18:07:18 +0800

    添加默认图片

--- app/src/main/java/com/kelly/banner/vpbanner2/VpBanner2Activity.java
+++ app/src/main/java/com/kelly/banner/vpbanner2/VpBanner2Activity.java
@@ -17,10 +17,14 @@ public class VpBanner2Activity extends AppCompatActivity {
     private FlyBanner mBannerNet;//加载网络图片
 
     private String[] mImagesUrl = {
-            "http://img4.imgtn.bdimg.com/it/u=2430963138,1300578556&fm=23&gp=0.jpg",
-            "http://img1.imgtn.bdimg.com/it/u=2755648979,3568014048&fm=23&gp=0.jpg",
-            "http://img0.imgtn.bdimg.com/it/u=2272739960,4287902102&fm=23&gp=0.jpg",
-            "http://img3.imgtn.bdimg.com/it/u=1078051055,1310741362&fm=23&gp=0.jpg"
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1487670814&di=68a37f51a0233a0ca396f8d661120e37&src=http://i.shangc.net/2017/0130/20170130053110183.jpg",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487680880654&di=8671166ebbd4e1c08c6d877a84c2ff65&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201702%2F08%2Fb9d736adf055b920d2cec2f53855137c.jpg",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487680880653&di=fcd2e7d22981cfd7fe1701bfcbf0c3f2&imgtype=0&src=http%3A%2F%2Fi2.hdslb.com%2Fbfs%2Farchive%2F3c1f8fe0970d2621f3c0e0983687b2d6e15b8910.jpg",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487680880650&di=6808bc859011952ec93a76d22dec2b65&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201702%2F20%2F44bf2e8e808e0b55a370974b5d1c83aa.jpg",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487680880644&di=45b0ee6a60431a51e2995eaf19295fca&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201702%2F16%2Fa1121812ce1587f5da5322a27de6ae7d.jpg",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487680880632&di=38421df82f00634adc8129f601174acf&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201702%2F16%2F8a7dfefb3c6999a68782b3e2df9c10ba.jpg",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487681072596&di=f04860ad05511004eb4beb90e0ffe811&imgtype=0&src=http%3A%2F%2Fwww.southyule.com%2Fd%2Ffile%2FTV%2F4%2F2017-02-10%2Ffdc717553b7a0004ce5370b24c59bef9.jpg",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487680880651&di=ee2455c250aae093c569642ec74b4ed0&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201702%2F16%2Fe5ce78f6c5c5124d4eac82edbc7df6eb.jpg"
     };
 
     @Override
@@ -40,10 +44,12 @@ public class VpBanner2Activity extends AppCompatActivity {
         mBannerLocal = (FlyBanner) findViewById(R.id.banner_1);
 
         List<Integer> images = new ArrayList<>();
-        images.add(R.drawable.img_1);
-        images.add(R.drawable.img_2);
-        images.add(R.drawable.img_3);
-        images.add(R.drawable.img_4);
+        images.add(R.mipmap.timg0);
+        images.add(R.mipmap.timg1);
+        images.add(R.mipmap.timg2);
+        images.add(R.mipmap.timg3);
+        images.add(R.mipmap.timg4);
+        images.add(R.mipmap.timg6);
         mBannerLocal.setImages(images);
 //        mBannerLocal.setPoinstPosition(FlyBanner.RIGHT);
 
